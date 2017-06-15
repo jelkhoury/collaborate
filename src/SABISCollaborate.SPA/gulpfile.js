d09cce238d69d87b64e9cd1b380318af784dedbf
hrizk <hiba.mrizk@gmail.com>
2017-06-15 19:16:27 +0300

    a

--- src/SABISCollaborate.SPA/gulpfile.js
+++ src/SABISCollaborate.SPA/gulpfile.js
@@ -5,7 +5,10 @@
 // other content removed
 
 gulp.task("sass", function () {
-    return gulp.src('wwwroot/css/main.scss')
+    return gulp.src(['wwwroot/css/main.scss', 'node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css'])
         .pipe(sass())
         .pipe(gulp.dest('wwwroot/css'));
 });
+
+
+//<link href=" ../node_modules/@angular/material/prebuilt-themes/pink-bluegrey.css" rel="stylesheet">
