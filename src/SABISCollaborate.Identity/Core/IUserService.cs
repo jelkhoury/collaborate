21a13c6e55f102866388d47093a2644312f1f6a7
josephelkhoury <joseph.elkhoury@outlook.com>
2017-08-20 22:21:12 +0300

    interface for UserService.cs

---
+++ src/SABISCollaborate.Identity/Core/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SABISCollaborate.Identity.Core
+{
+    public interface IUserService
+    {
+        User AutoProvisionUser(string provider, string userId, List<Claim> claims);
+
+        User FindByExternalProvider(string provider, string userId);
+
+        User FindBySubjectId(string subjectId);
+
+        User FindByUsername(string username);
+
+        bool ValidateCredentials(string username, string password);
+    }
+}
---
+++ src/SABISCollaborate.Identity/Core/UserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SABISCollaborate.Identity.Core
+{
+    public class UserService : IUserService
+    {
+        public User AutoProvisionUser(string provider, string userId, List<Claim> claims)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User FindByExternalProvider(string provider, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User FindBySubjectId(string subjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User FindByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
